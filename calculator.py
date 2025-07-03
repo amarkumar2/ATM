e9bf3a8de0d2dee6a8f8854e8d7d174491f3f484
amarkumar2 <amarkumarmahtha09@gmail.com>
2025-07-03 20:52:09 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# class calculator():
+
+#     def __init__(self,n):
+#         self.n = n
+#     def square(self):
+#         print(f'the square is {self.n * self.n}')
+#     def cube(self):
+#         print(f'the cube is {self.n * self.n*self.n}')
+#     def squareroot(self):
+#         print(f'the squareroot is {self.n **1/2}')
+
+#     @staticmethod
+#     def hi():
+#         print('hey there welcome👍:')
+# a = calculator()
+# a.hi()
+# a.cube()
+# a.square()
+
+class train():
+    def __init__(self,name,occupy,branch):
+        self.name = name
+        self.occupy = occupy
+        self.branch = branch
+    
+    def lol():
+        print(f'train name is {name}')
